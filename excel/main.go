3e9c78f52217991aba6e0f1b032a69c9b805a7ce
khanhtc1202 <khanhtc1202@gmail.com>
2019-12-03 18:26:05 +0900

    invest

--- excel/main.go
+++ excel/main.go
@@ -1,42 +1,26 @@
 package main
 
 import (
+	"log"
 	"fmt"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 func main() {
-	f := excelize.NewFile()
-	// Create a new sheet.
-	index := f.NewSheet("Sheet2")
-	// Set value of a cell.
-	f.SetCellValue("Sheet2", "A2", "Hello world.")
-	f.SetCellValue("Sheet1", "B2", 100)
-	// Set active sheet of the workbook.
-	f.SetActiveSheet(index)
-	// Save xlsx file by the given path.
-	err := f.SaveAs("./Book1.xlsx")
-	if err != nil {
-		fmt.Println(err)
-	}
+	source := read("sample.xls")
+
+	sheets := source.GetSheetMap()
+	// get title row
+	rows, err := source.GetRows
 }
 
-func read() {
-	f, err := excelize.OpenFile("./Book1.xlsx")
+func read(name string) *excelize.File {
+	f, err := excelize.OpenFile(name)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalf("Error on open excel source:\n%v", err)
 	}
-	// Get value from cell by given worksheet name and axis.
-	cell := f.GetCellValue("Sheet1", "B2")
-	fmt.Println(cell)
-	// Get all the rows in the Sheet1.
-	rows := f.GetRows("Sheet1")
-	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, "\t")
-		}
-		fmt.Println()
-	}
+	return f
 }
+
+
